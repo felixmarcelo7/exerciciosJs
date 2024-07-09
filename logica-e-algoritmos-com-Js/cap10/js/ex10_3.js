8341922628be733b81afa94acafd7a01bb526423
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2024-07-08 21:41:36 -0300

    revisão do ex10_3 feita

--- logica-e-algoritmos-com-Js/cap10/js/ex10_3.js
+++ logica-e-algoritmos-com-Js/cap10/js/ex10_3.js
@@ -78,3 +78,78 @@ tbFilmes.addEventListener("click", (e) => {
     }
   }
 });
+
+/* Revisão
+
+const frm = document.querySelector("form");
+const tbFilmes = document.querySelector("table");
+
+frm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const titulo = frm.inFilme.value;
+  const genero = frm.inGenero.value;
+
+  inserirLinha(titulo, genero);
+  gravaFilmes(titulo, genero);
+
+  frm.reset();
+  frm.inFilme.focus();
+});
+
+const inserirLinha = (titulo, genero) => {
+  const linha = tbFilmes.insertRow(-1);
+  
+  const col1 = linha.insertCell(0);
+  const col2 = linha.insertCell(1);
+  const col3 = linha.insertCell(2);
+
+  col1.innerText = titulo;
+  col2.innerText = genero;
+  col3.innerHTML = "<i class='exclui' title='Excluir'>&#10008</i>";
+};
+
+const gravaFilmes = (titulo, genero) => {
+  if(localStorage.getItem("filmesTitulo")) {
+    const filmesTitulo = localStorage.getItem("filmesTitulo") + ";" + titulo;
+    const filmesGenero = localStorage.getItem("filmesGenero") + ";" + genero;
+    localStorage.setItem("filmesTitulo", filmesTitulo);
+    localStorage.setItem("filmesGenero", filmesGenero);
+  } else {
+    localStorage.setItem("filmesTitulo", titulo);
+    localStorage.setItem("filmesGenero", genero);
+  }
+};
+
+window.addEventListener("load", () => {
+  if(localStorage.getItem("filmesTitulo")){
+    const titulos = localStorage.getItem("filmesTitulo").split(";");
+    const generos = localStorage.getItem("filmesGenero").split(";");
+
+    for(let i = 0; i < titulos.length; i++) {
+      inserirLinha(titulos[i], generos[i]);
+    }
+  }
+});
+
+tbFilmes.addEventListener("click", e => {
+  if(e.target.classList.contains("exclui")) {
+    const titulo = e.target.parentElement.parentElement.children[0].innerText;
+
+    if(confirm(`Confirma a Exclusão do filme "${titulo}"`)) {
+      e.target.parentElement.parentElement.remove();
+
+      localStorage.removeItem("filmesTitulo");
+      localStorage.removeItem("filmesGenero");
+
+      for(let i = 1; i < tbFilmes.rows.length; i++) {
+        const auxTitulo = tbFilmes.rows[i].cells[0].innerText;
+        const auxGenero = tbFilmes.rows[i].cells[1].innerText;
+
+        gravaFilmes(auxTitulo, auxGenero);
+      }
+    }
+  }
+});
+
+*/
