6f0c348de1ebdbaf23165e4f769a9cfc4367f828
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2024-08-05 13:47:55 -0300

    primeira parte do .js do ex11_1 pronto

---
+++ logica-e-algoritmos-com-Js/cap11/js/ex11_1.js
@@ -0,0 +1,87 @@
+const frm = document.querySelector("form");
+const respCavalo = document.querySelector("#outCavalo");
+const listaCavalos = document.querySelector("pre");
+
+const CAVALOS = ["Marujo", "Tordilho", "Belga", "Twister", "Jade", "Lucky"];
+
+const apostas = [];
+
+const obterCavalo = numCavalo => { //pega o nome do cavalo que vai apostar
+    const cavalo = numCavalo - 1;
+    return CAVALOS[cavalo];
+};
+
+const quantApostas = (numCavalo) => { //conta a quantidade de apostas que foram feitas no cavalo
+    let cont = 0;
+
+    apostas.forEach((aposta) => {
+        if (aposta.numCavalo == numCavalo) {
+            cont++;
+        }
+    });
+
+    return cont;
+};
+
+const somaApostas = (numCavalo) => { // soma as apostas feitas no cavalo escolhido
+    let soma = 0;
+
+    apostas.forEach((aposta) => {
+        if (aposta.numCavalo == numCavalo) {
+            soma += aposta.valAposta;
+        }
+    });
+
+    return soma;
+};
+
+const validaCavalo = (numCavalo) => { // se a aposta for em um número maior que o de cavalos retorna falso
+    
+    if(numCavalo  <= CAVALOS.length && numCavalo > 0) {
+        return true;
+    }
+
+    return false;
+}
+
+frm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const numCavalo = Number(frm.inCavalo.value);
+    const valAposta = Number(frm.inAposta.value);
+
+    apostas.push({ numCavalo, valAposta }); //adicionando dentro do objeto
+
+    listaCavalos.innerText = `Apostas Realizadas\n${"-".repeat(25)}\n`;
+
+    let lista = "";
+    for (const aposta of apostas) {
+        const { numCavalo, valAposta } = aposta;
+        lista += `Nº ${numCavalo} ${obterCavalo(numCavalo)} - R$: ${valAposta.toFixed(2)}\n`; //cria a lista de apostas
+    }
+
+    listaCavalos.innerText += lista;//mostra a lista na tela
+    frm.reset();
+    frm.inCavalo.focus();
+    respCavalo.innerText = "";
+});
+
+frm.inCavalo.addEventListener("blur", () => {
+    //mostra o nome do cavalo que vai ser apostado e o total de apostas
+    const numCavalo = Number(frm.inCavalo.value);
+
+    if(numCavalo == "") {
+        respCavalo.innerText = "";
+        return;
+    }
+
+    if(!validaCavalo(numCavalo)) {
+        alert(`Número inválido. Tente novamente! Apenas ${CAVALOS.length} cavalos no páreo` );
+        respCavalo.innerText = "";
+        return;
+    }
+
+    respCavalo.innerText = `${obterCavalo(numCavalo)} (Apostas: ${quantApostas(numCavalo)} -`;
+    respCavalo.innerText += ` R$: ${somaApostas(numCavalo).toFixed(2)})`;
+});
+
