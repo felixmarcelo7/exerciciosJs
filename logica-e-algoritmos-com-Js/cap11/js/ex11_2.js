77c20d093126a8c3e5ac162d0a2429b40f42d2fb
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2024-08-12 22:19:55 -0300

    segunda parte do script do ex11_2 pronto

--- logica-e-algoritmos-com-Js/cap11/js/ex11_2.js
+++ logica-e-algoritmos-com-Js/cap11/js/ex11_2.js
@@ -5,43 +5,75 @@ const POLTRONAS = 240; //const com o número de poltronas do teatro
 const reservadas = []; //vetor com as poltronas reservadas pelos clientes
 
 window.addEventListener("load", () => {
-//operador ternário: se houver dados salvos em localStorage, faz um split(";") e
-//atribui esses dados ao array, caso contráruio, o array é inicializado vazio
-const ocupadas = localStorage.getItem("teatroOcupadas") 
-    ? localStorage.getItem("teatroOcupadas").split(";") 
+  //operador ternário: se houver dados salvos em localStorage, faz um split(";") e
+  //atribui esses dados ao array, caso contráruio, o array é inicializado vazio
+  const ocupadas = localStorage.getItem("teatroOcupadas")
+    ? localStorage.getItem("teatroOcupadas").split(";")
     : [];
 
-    //repetição para montar o n° de poltonas
-    for(let i = 1; i <= POLTRONAS; i++) {
-        const figure = document.createElement("figure");
-        const imgStatus = document.createElement("img");
+  //repetição para montar o n° de poltonas
+  for (let i = 1; i <= POLTRONAS; i++) {
+    const figure = document.createElement("figure");
+    const imgStatus = document.createElement("img");
 
-        //se a posição const em ocupadas, exibe a imgem ocupada, se não, disponível
-        imgStatus.src = ocupadas.includes(i.toString()) 
-            ? "img/ocupada.jpg"
-            : "img/disponivel.jpg";
-            
-        imgStatus.className = "poltrona"; //classe com dimensão da img
-        const figureCap = document.createElement("figcation");
-        figureCap.style = "display: flex";
+    //se a posição const em ocupadas, exibe a imgem ocupada, se não, disponível
+    imgStatus.src = ocupadas.includes(i.toString())
+      ? "img/ocupada.jpg"
+      : "img/disponivel.jpg";
 
-        //quantidade de zeros antes do número da poltrona
-        const zeros = i < 10 ? "00" : i < 100 ? "0" : "";
+    imgStatus.className = "poltrona"; //classe com dimensão da img
+    const figureCap = document.createElement("figcation");
+    figureCap.style = "display: flex";
 
-        const num = document.createTextNode(`[${zeros}${i}]`);
+    //quantidade de zeros antes do número da poltrona
+    const zeros = i < 10 ? "00" : i < 100 ? "0" : "";
 
-        figureCap.appendChild(num);
-        figure.appendChild(imgStatus);
-        figure.appendChild(figureCap);
+    const num = document.createTextNode(`[${zeros}${i}]`);
 
-        //se i módulo 24 == 12 (é o corredor: define margem direita 60px)
-        if(i % 12 == 0) figure.style.marginRight = "60px";
+    figureCap.appendChild(num);
+    figure.appendChild(imgStatus);
+    figure.appendChild(figureCap);
 
-        dvPalco.appendChild(figure); //indica que firure é filha de dvPalco
+    //se i módulo 24 == 12 (é o corredor: define margem direita 60px)
+    if (i % 12 == 0) figure.style.marginRight = "60px";
 
-        //se i modulo 24 == 0: o comando após && será executado (insere quebra de linha)
-        (i % 24 == 0) && dvPalco.appendChild(document.createElement("br"));
-        
-    }
+    dvPalco.appendChild(figure); //indica que firure é filha de dvPalco
+
+    //se i modulo 24 == 0: o comando após && será executado (insere quebra de linha)
+    i % 24 == 0 && dvPalco.appendChild(document.createElement("br"));
+  }
 });
 
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const poltrona = frm.inPoltrona.value;
+
+  if (poltrona > POLTRONAS) {
+    //valida
+    alert("Informe m número de poltrona válido");
+    frm.inPoltrona.focus();
+    return;
+  }
+
+  const ocupadas = localStorage.getItem("teatroOcupadas")
+    ? localStorage.getItem("teatroOcupadas").split(";")
+    : [];
+
+  //se a poltrona estiver ocupada
+  if (ocupadas.includes(poltrona.toString())) {
+    alert(`A poltrona ${poltrona} já está ocupada...`);
+    frm.inPoltrona.value = "";
+    frm.inPoltrona.focus();
+    return;
+  }
+
+  const imgPoltrona = dvPalco.querySelectorAll("img")[poltrona - 1];
+
+  imgPoltrona.src = "img/reservada.jpg";
+
+  reservadas.push(poltrona);
+
+  frm.inPoltrona.value = "";
+  frm.inPoltrona.focus();
+});
